2f03becaf248d4457ffae2cdc0e0d0af9b64e946
Julian Martinez <Julianmartinez_93@hotmail.com>
2025-10-04 18:12:51 -0300

    add entities

---
+++ ApiChallenge/Data/Entities/Domicilio/Domicilio.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiChallenge.Data.Entities.Domicilio;
+
+public class Domicilio
+{
+    public int Id { get; set; }
+    public int UserioId { get; set; }
+    public string? Calle { get; set; }
+    public string? Numero { get; set; }
+    public string? Provinicia { get; set; }
+    public string? Ciudad { get; set; }
+    public DateTime? FechaCreacion { get; set; }
+}
---
+++ ApiChallenge/Data/Entities/User/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ApiChallenge.Data.Entities.User;
+
+public class User
+{
+    public int Id { get; set; }
+    [NotNull, MaxLength(300)]
+    public string? Nombre { get; set; }
+    [NotNull, MaxLength(300), EmailAddress]
+    public string? Email { get; set; }
+    public DateTime? FechaCreacion { get; set; }
+}
