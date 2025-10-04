d4c336ef78df651ef063a98d1a6995ccbd1013f6
Julian Martinez <Julianmartinez_93@hotmail.com>
2025-10-04 18:32:04 -0300

    config dbset and add to program

---
+++ ApiChallenge/Data/ChallengeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiChallenge.Data.Entities.Domicilio;
+using ApiChallenge.Data.Entities.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiChallenge.Data;
+
+public class ChallengeDbContext : DbContext
+{
+    public virtual DbSet<User> Usuarios { get; set; }
+
+    public virtual DbSet<Domicilio> Domicilio { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySQL("server=127.0.0.1;port=3306;database=codeChallenge;user=root;password=admin");
+}
--- ApiChallenge/Program.cs
+++ ApiChallenge/Program.cs
@@ -1,3 +1,5 @@
+using ApiChallenge.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ChallengeDbContext>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    ChallengeDbContext context = scope.ServiceProvider.GetRequiredService<ChallengeDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,9 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapGet("/", () => "Hello World!");
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
